d7a12de0644b4838d1f13b71da4603284c067091
vineethdevadiga8468@gmail.com <vineethdevadiga8468@gmail.com>
2026-04-02 15:04:12 +0530

    simple addition using node js

---
+++ learning_node_js/addnum.js
@@ -0,0 +1,5 @@
+const num1 =Number(process.argv[2]);
+const num2 =Number(process.argv[3]);
+
+console.log("Sum:",num1+num2);
+// this is how you take up 2 numbers as argument and convert the given char argument into int via Number and make it const and then print it out 
